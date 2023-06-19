b63dd76f2a6eaf5a9aaf403b7e09d7d75f9f5c2f
chiranjivi <chiranjivikeshav1@gmail.com>
2023-06-19 16:00:37 +0530

    second commit

--- server/settings.py
+++ server/settings.py
@@ -77,13 +77,15 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(default='postgres://chiranjivikeshav:5UTPr5CaDxScsLQJg54SOy7boWQmq9Rw@dpg-ci7vhd18g3nfucaujnog-a.singapore-postgres.render.com/etplatform', ssl_require=True)
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
