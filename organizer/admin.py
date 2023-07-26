465efaf2708336c98d1041c656fcdcf4bea9ec17
chiranjivi <chiranjivikeshav1@gmail.com>
2023-07-26 07:24:47 +0530

    Merge etplatform  branch into main

--- organizer/admin.py
+++ organizer/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
-<<<<<<< HEAD
-
-# Register your models here.
-=======
 from .models import Eventdetails,Ticket,Organizer
 # Register your models here.
 admin.site.register(Eventdetails)
 admin.site.register(Ticket)
 admin.site.register(Organizer)
->>>>>>> etplatform
