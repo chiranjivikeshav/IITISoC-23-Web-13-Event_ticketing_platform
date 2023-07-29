a422c1ab25e06a3118599c4ae700b396095937eb
chiranjivi <chiranjivikeshav1@gmail.com>
2023-07-30 01:32:35 +0530

    fixing issue after deploy

--- server/settings.py
+++ server/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
 MEDIA_URL="/media/"
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
