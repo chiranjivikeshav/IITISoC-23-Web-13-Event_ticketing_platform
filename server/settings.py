49de83bed8ff07d6bb5cfc49843e447e38ba22df
chiranjivi <chiranjivikeshav1@gmail.com>
2023-07-29 23:28:36 +0530

    fixing issue after deploy

--- server/settings.py
+++ server/settings.py
@@ -135,11 +135,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+MEDIA_ROOT= os.path.join(BASE_DIR, "media")
+MEDIA_URL="/media/"
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
 
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
