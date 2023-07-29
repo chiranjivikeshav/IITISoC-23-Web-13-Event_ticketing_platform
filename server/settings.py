5f6e6feafb7f518404a4a52b14510d8e81cc69e4
chiranjivi <chiranjivikeshav1@gmail.com>
2023-07-29 23:45:04 +0530

    fixing issue after deploy

--- server/settings.py
+++ server/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,8 +136,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = 'static/'
-MEDIA_ROOT= os.path.join(BASE_DIR, "media")
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
 MEDIA_URL="/media/"
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
