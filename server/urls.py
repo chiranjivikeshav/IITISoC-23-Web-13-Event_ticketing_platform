bca992a52f1c39245ffc71157c0743fde5ac2c4e
chiranjivi <chiranjivikeshav1@gmail.com>
2023-07-29 11:40:58 +0530

    fixing issue after deploy

--- server/urls.py
+++ server/urls.py
@@ -23,3 +23,5 @@ urlpatterns = [
     path('',include('organizer.urls')),
     path('user/',include('user.urls')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
